ec7bcd12dd90e5bc4a73c5dffa23dadfe4d0f99a
James W. Kimani <jwkimani@users.noreply.github.com>
2014-09-09 16:21:11 -0400

    Create postgres-classic-models-drop-tables.sql

---
+++ Database/postgres-classic-models-drop-tables.sql
@@ -0,0 +1,8 @@
+drop TABLE "orderdetails";
+drop TABLE "products";
+drop TABLE "productlines" ;
+drop TABLE "payments" ;
+drop TABLE "orders";
+drop TABLE "customers";
+drop TABLE "employees";
+drop TABLE "offices" ;
