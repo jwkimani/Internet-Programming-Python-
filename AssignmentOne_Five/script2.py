0a0abc294c8852f9cbab05fd9433dfd8e921be62
James W. Kimani <jkimani2@gmail.com>
2014-09-22 11:33:31 -0400

    Amendments on AssignmentTwo\Script1

--- AssignmentOne_Five/script2.py
+++ AssignmentOne_Five/script2.py
@@ -23,7 +23,7 @@ def main():
     header = "{:20} {:<50} {:<20} {:<40} {:<30} {:<20} {:<10}".format("Product Code", "Product Name",
                                                                       "Product Scale", "Product Vendor", "Quantity in "
                                                                         "Stock", "Buy Price", "M.S.R.P")
-    print('\n',productLine)
+    print('\nThis is a list of all the %s' % productLine)
     print("{:=^197}".format('='))
     print(header)
     print("{:=^197}".format('='))
--- Assignment_Two/script1.py
+++ Assignment_Two/script1.py
@@ -1,1 +1,35 @@
 __author__ = 'jkimani2'
+
+'''
+The first script will list all customers with the following information:
+
+Customer number
+Customer name
+Total of payments made
+Total value of all orders made
+Credit limit
+Amount of available credit (credit limit plus the total payments made minus the total value of all orders made)
+The list should be in a tabular format with data lined up in columns.
+'''
+
+import psycopg2
+
+conn = psycopg2.connect(database="birt", user = "birt", password="birt")
+get_customerNumber = "select customernumber from customers"
+crs = conn.cursor()
+crs.execute(get_customerNumber)
+results = crs.fetchall()
+crs2 = conn.cursor()
+crs3 = conn.cursor()
+
+cusnum = []
+for a in range(0,len(results)):
+    cusnum.append(results[a][0])
+
+for p in cusnum:
+    crs2.execute("select amount from payments where customernumber = %i" % (p))
+    for c in crs2:
+        print(c, p)
+
+for a in crs2:
+    print(a)
